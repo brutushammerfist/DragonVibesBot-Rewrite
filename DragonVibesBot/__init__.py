e881e397fd0e24ff573a5986c5d70dbed01488ef
BrutusHammerfist <mperr006@odu.edu>
2019-09-14 02:56:20 +0000

    New to flask, does this need to match?

--- DragonVibesBot/__init__.py
+++ DragonVibesBot/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 #socketio = SocketIO(app)
 
-from app import routes, models
+from DragonVibesBot import routes, models
 
 #if __name__ == '__main__':
 #    socketio.run(app)
